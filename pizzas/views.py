99bf275b6b426eb57344b1ef5ef6dc12197c5312
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2024-02-14 11:37:26 +0000

    Add views and urls for pizza menu and a base pizza.html file

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def all_pizzas(request):
+    """ A view to show all pizzas, including sorting and search queries """
+
+    # You would typically get your Pizza objects from the database here.
+    # For example:
+    # pizzas = Pizza.objects.all()
+
+    # For now, we will just pass an empty context since we don't have a Pizza model yet.
+    context = {}
+
+    return render(request, 'pizzas/pizzas.html', context)
