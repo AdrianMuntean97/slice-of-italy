922b98a8e5c2d08a8feb524d019d1f2b6e0464ac
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2024-02-22 18:24:53 +0000

    Changed the home view to display the correct category in the menu bar

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from pizzas.models import Category
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
+    all_categories = Category.objects.all()  # Get all category objects
 
-    return render(request, 'home/index.html')
+    context = {
+        'all_categories': all_categories,  # Pass all categories to the template
+    }
+
+    return render(request, 'home/index.html', context)
